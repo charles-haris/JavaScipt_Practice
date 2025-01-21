c793b6a9681ff2d1aba8b0bc4098c55868caf4e5
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-01-21 16:41:10 +0000

    improvement on your old code
    looking at the name of your project, I was curious, And I decided to give some tips, on how to ask the name and the age properly in JS

--- nameAndAge.js
+++ nameAndAge.js
@@ -1,2 +1,16 @@
+//generally the names of your variables must be speaking names 
+let name = prompt("What is your name?");
+console.log("Your name is", name);
+
+let age = prompt("How old are you?");
+age = parseInt(age);
+
+if (!isNaN(age)) {
+  console.log("Your age is", age);
+} else {
+  console.log("Invalid age entered.");
+}
+
+//this is your previous code
 let a = parseInt(prompt("What is your name?"))
 console.log("How are you doing", a)
